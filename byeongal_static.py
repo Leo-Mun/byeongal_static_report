a6d142d9dc87c8fbc6cb2f52acb7437e8361245c
Young Jae Kim <corea_kyj@naver.com>
2018-12-04 23:16:01 +0900

    섹션 정보 및 리소스 정보 가져오는 기능 추가

--- byeongal_static.py
+++ byeongal_static.py
@@ -1,4 +1,4 @@
-import sys
+﻿import sys
 import os
 import pefile
 import peutils
@@ -8,17 +8,17 @@ import magic
 import datetime
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
-_USER_DB = os.path.join(_ROOT, 'signatures', 'userdb_panda.txt')
+_USER_DB = os.path.join(_ROOT, 'signatures', 'userdb.txt')
 
 def print_help () :
     pass
 
 def isfile(file_path):
-	if os.path.isfile(file_path):
-		return True
-	else:
-		print("No file found.")
-		exit()
+    if os.path.isfile(file_path):
+        return True
+    else :
+        print("No file found.")
+        exit()
 
 def get_imphash( pe ):
     try :
@@ -66,6 +66,54 @@ def get_packer_info( pe ) :
     return array
     pass
 
+def get_sections_number( pe ):
+    return pe.FILE_HEADER.NumberOfSections
+
+def get_resources_info( pe ) :
+    res_array = []
+    try :
+        for resource_type in pe.DIRECTORY_ENTRY_RESOURCE.entries:
+            if resource_type.name is not None:
+                name = "%s" % resource_type.name
+            else:
+                name = "%s" % pefile.RESOURCE_TYPE.get(resource_type.struct.Id)
+            if name == None:
+                name = "%d" % resource_type.struct.Id
+            if hasattr(resource_type, 'directory') :
+                for resource_id in resource_type.directory.entries :
+                    if hasattr(resource_id, 'directory'):
+                        for resource_lang in resource_id.directory.entries:
+                            try:
+                                data = pe.get_data(resource_lang.data.struct.OffsetToData, resource_lang.data.struct.Size).decode('utf-8')
+                            except:
+                                pass
+                            lang = pefile.LANG.get(resource_lang.data.lang, '*unknown*')
+                            sublang = pefile.get_sublang_name_for_lang(resource_lang.data.lang, resource_lang.data.sublang)
+                            res_array.append({"name": name, "data": data, "offset": hex(resource_lang.data.struct.OffsetToData),"size": resource_lang.data.struct.Size, "language": lang, "sublanguage": sublang})
+    except :
+        pass
+    return res_array
+
+def get_sections_info( pe ) :
+    array = []
+    for section in pe.sections:
+        section.get_entropy()
+        if section.SizeOfRawData == 0 or (section.get_entropy() > 0 and section.get_entropy() < 1) or section.get_entropy() > 7:
+            suspicious = True
+        else:
+            suspicious = False
+
+        scn = section.Name
+        md5 = section.get_hash_md5()
+        sha1 = section.get_hash_sha1()
+        spc = suspicious
+        va = hex(section.VirtualAddress)
+        vs = hex(section.Misc_VirtualSize)
+        srd = section.SizeOfRawData
+        array.append({"name": scn.decode(), "hash_md5": md5, "hash_sha1": sha1, "suspicious": spc, "virtual_address": va, "virtual_size": vs, "size_raw_data": srd})
+
+    return array
+
 def run( file_path ) :
     pe = pefile.PE(file_path)
     json_obj = dict()
@@ -87,7 +135,14 @@ def run( file_path ) :
     json_obj['pe_info'] = dict()
     ## Compile Time
     json_obj['pe_info']['compile_time'] = get_compile_time(pe)
+    ## Packer Info
     json_obj['pe_info']['packer_info'] = get_packer_info( pe )
+    ## Sessions Number
+    json_obj['pe_info']['sections_number'] = get_sections_number(pe)
+    ## Resources
+    json_obj['pe_info']['resources_info'] = get_resources_info(pe)
+    ## Sections Info
+    json_obj['pe_info']['sections_ino'] = get_sections_info(pe)
 
     # Save report file
     with open("{}.json".format(json_obj['hash']['sha256']), 'w') as f :
@@ -95,7 +150,8 @@ def run( file_path ) :
 
 if __name__ == '__main__' :
     if len(sys.argv) == 1 :
-        print_help()
+        #print_help()
+        run("atom.exe")
         exit(0)
     if len(sys.argv) == 2 :
         file_path = sys.argv[1]
