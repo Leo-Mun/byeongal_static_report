22d623c16b782e5020ddeda67fec684788b66e16
Young Jae KIm <corea_kyj@naver.com>
2019-02-04 14:48:32 +0900

    some error is fixed and some feature convert machinefreindly

--- byeongal_static.py
+++ byeongal_static.py
@@ -5,8 +5,6 @@ import peutils
 import hashlib
 import json
 import magic
-import datetime
-import chardet
 import yara
 import string
 import ssdeep
@@ -16,76 +14,6 @@ _ROOT = os.path.abspath(os.path.dirname(__file__))
 _USER_DB = os.path.join(_ROOT, 'signatures', 'userdb.txt')
 _APIALERT = os.path.join(_ROOT, 'signatures', 'apialertv6.1.txt')
 _ANTIDEBUG = os.path.join(_ROOT, 'signatures', 'AntiDebugging.yara')
-
-_MACHINE_TYPES = {
-    0x0: 'IMAGE_FILE_MACHINE_UNKNOWN',
-    0x14c: 'IMAGE_FILE_MACHINE_I386',
-    0x162: 'IMAGE_FILE_MACHINE_R3000',
-    0x166: 'IMAGE_FILE_MACHINE_R4000',
-    0x168: 'IMAGE_FILE_MACHINE_R10000',
-    0x169: 'IMAGE_FILE_MACHINE_WCEMIPSV2',
-    0x184: 'IMAGE_FILE_MACHINE_ALPHA',
-    0x1a2: 'IMAGE_FILE_MACHINE_SH3',
-    0x1a3: 'IMAGE_FILE_MACHINE_SH3DSP',
-    0x1a4: 'IMAGE_FILE_MACHINE_SH3E',
-    0x1a6: 'IMAGE_FILE_MACHINE_SH4',
-    0x1a8: 'IMAGE_FILE_MACHINE_SH5',
-    0x1c0: 'IMAGE_FILE_MACHINE_ARM',
-    0x1c2: 'IMAGE_FILE_MACHINE_THUMB',
-    0x1c4: 'IMAGE_FILE_MACHINE_ARMNT',
-    0x1d3: 'IMAGE_FILE_MACHINE_AM33',
-    0x1f0: 'IMAGE_FILE_MACHINE_POWERPC',
-    0x1f1: 'IMAGE_FILE_MACHINE_POWERPCFP',
-    0x200: 'IMAGE_FILE_MACHINE_IA64',
-    0x266: 'IMAGE_FILE_MACHINE_MIPS16',
-    0x284: 'IMAGE_FILE_MACHINE_ALPHA64',
-    0x284: 'IMAGE_FILE_MACHINE_AXP64',
-    0x366: 'IMAGE_FILE_MACHINE_MIPSFPU',
-    0x466: 'IMAGE_FILE_MACHINE_MIPSFPU16',
-    0x520: 'IMAGE_FILE_MACHINE_TRICORE',
-    0xcef: 'IMAGE_FILE_MACHINE_CEF',
-    0xebc: 'IMAGE_FILE_MACHINE_EBC',
-    0x8664: 'IMAGE_FILE_MACHINE_AMD64',
-    0x9041: 'IMAGE_FILE_MACHINE_M32R',
-    0xc0ee: 'IMAGE_FILE_MACHINE_CEE',
-}
-
-_IMAGE_CHARACTERISTICS = {
-    0x1 : 'IMAGE_FILE_RELOCS_STRIPPED',
-    0x2 : 'IMAGE_FILE_EXECUTABLE_IMAGE',
-    0x4 : 'IMAGE_FILE_LINE_NUMS_STRIPPED',
-    0x8 : 'IMAGE_FILE_LOCAL_SYMS_STRIPPED',
-    0x10 : 'IMAGE_FILE_AGGRESIVE_WS_TRIM',
-    0x20 : 'IMAGE_FILE_LARGE_ADDRESS_AWARE',
-    0x40 : 'IMAGE_FILE_16BIT_MACHINE',
-    0x80 : 'IMAGE_FILE_BYTES_REVERSED_LO',
-    0x100 : 'IMAGE_FILE_32BIT_MACHINE',
-    0x200 : 'IMAGE_FILE_DEBUG_STRIPPED',
-    0x400 : 'IMAGE_FILE_REMOVABLE_RUN_FROM_SWAP',
-    0x800 : 'IMAGE_FILE_NET_RUN_FROM_SWAP',
-    0x1000 : 'IMAGE_FILE_SYSTEM',
-    0x2000 : 'IMAGE_FILE_DLL',
-    0x4000 : 'IMAGE_FILE_UP_SYSTEM_ONLY',
-    0x8000 : 'IMAGE_FILE_BYTES_REVERSED_HI',
-}
-
-_DLL_CHARACTERISTICS = {
-    0x1:'IMAGE_LIBRARY_PROCESS_INIT',
-    0x2:'IMAGE_LIBRARY_PROCESS_TERM',
-    0x4:'IMAGE_LIBRARY_THREAD_INIT',
-    0x8:'IMAGE_LIBRARY_THREAD_TERM',
-    0x20:'IMAGE_DLLCHARACTERISTICS_HIGH_ENTROPY_VA',
-    0x40:'IMAGE_DLLCHARACTERISTICS_DYNAMIC_BASE',
-    0x80:'IMAGE_DLLCHARACTERISTICS_FORCE_INTEGRITY',
-    0x100:'IMAGE_DLLCHARACTERISTICS_NX_COMPAT',
-    0x200:'IMAGE_DLLCHARACTERISTICS_NO_ISOLATION',
-    0x400:'IMAGE_DLLCHARACTERISTICS_NO_SEH',
-    0x800:'IMAGE_DLLCHARACTERISTICS_NO_BIND',
-    0x1000:'IMAGE_DLLCHARACTERISTICS_APPCONTAINER',
-    0x2000:'IMAGE_DLLCHARACTERISTICS_WDM_DRIVER',
-    0x4000:'IMAGE_DLLCHARACTERISTICS_GUARD_CF',
-    0x8000:'IMAGE_DLLCHARACTERISTICS_TERMINAL_SERVER_AWARE'
-}
 
 def print_help () :
     pass
@@ -120,17 +48,10 @@ def get_hash(file_path):
     md5 = md5.hexdigest()
     sha1 = sha1.hexdigest()
     sha256 = s256.hexdigest()
-
     return md5, sha1, sha256
 
 def get_compile_time(pe) :
-    # timestamp
-    tstamp = pe.FILE_HEADER.TimeDateStamp
-    try:
-        tsdate = datetime.datetime.fromtimestamp(tstamp).strftime("%Y-%m-%d %H:%M:%S")
-    except:
-        tsdate = str(tstamp) + " [Invalid date]"
-    return tsdate
+    return pe.FILE_HEADER.TimeDateStamp
 
 def get_packer_info( pe ) :
     signatures = peutils.SignatureDatabase(_USER_DB)
@@ -141,7 +62,6 @@ def get_packer_info( pe ) :
             if item[0] not in array:
                 array.append(item[0])
     return array
-    pass
 
 def get_sections_number( pe ):
     return pe.FILE_HEADER.NumberOfSections
@@ -161,22 +81,11 @@ def get_resources_info( pe ) :
                 for resource_id in resource_type.directory.entries :
                     if hasattr(resource_id, 'directory'):
                         for resource_lang in resource_id.directory.entries:
-                            data = ""
-                            try:
-                                raw_data = pe.get_data(resource_lang.data.struct.OffsetToData, resource_lang.data.struct.Size)
-                                for char in raw_data :
-                                    char = chr(char)
-                                    if char in printable :
-                                        data += char
-                                #encoding_option = chardet.detect(raw_data)['encoding']
-                                #if encoding_option != None :
-                                #    data = raw_data.decode(encoding_option, 'ignore').strip()
-                            except Exception as e:
-                                print(e)
-                                pass
+                            raw_data = pe.get_data(resource_lang.data.struct.OffsetToData, resource_lang.data.struct.Size)
+                            raw_data = [ format(i, '#04x') for i in raw_data ]
                             lang = pefile.LANG.get(resource_lang.data.lang, '*unknown*')
                             sublang = pefile.get_sublang_name_for_lang(resource_lang.data.lang, resource_lang.data.sublang)
-                            res_array.append({"name": name, "data": data, "offset": hex(resource_lang.data.struct.OffsetToData),"size": resource_lang.data.struct.Size, "language": lang, "sublanguage": sublang})
+                            res_array.append({"name": name, "data": raw_data, "offset": hex(resource_lang.data.struct.OffsetToData),"size": resource_lang.data.struct.Size, "language": lang, "sublanguage": sublang})
     except :
         pass
     return res_array
@@ -196,7 +105,7 @@ def get_sections_info( pe ) :
         vs = hex(section.Misc_VirtualSize)
         srd = hex(section.SizeOfRawData)
         entropy = section.get_entropy()
-        array.append({"name": scn.decode().replace("\u0000",""), "hash_md5": md5, "hash_sha1": sha1, "suspicious": spc, "virtual_address": va, "virtual_size": vs, "size_raw_data": srd, "entropy" : entropy})
+        array.append({"name": scn.decode().strip(' \t\r\n\0'), "hash_md5": md5, "hash_sha1": sha1, "suspicious": spc, "virtual_address": va, "virtual_size": vs, "size_raw_data": srd, "entropy" : entropy})
 
     return array
 
@@ -207,18 +116,14 @@ def get_import_function( pe ) :
     try:
         for entry in pe.DIRECTORY_ENTRY_IMPORT:
             dll = entry.dll
-            encoding_option = chardet.detect(dll)['encoding']
-            if encoding_option == None:
+            if dll == None :
                 continue
-            dll = dll.decode(encoding_option, 'ignore').replace("\u0000","")
+            dll = dll.decode().strip(' \t\r\n\0')
             for imp in entry.imports:
                 address = hex(imp.address)
                 function = imp.name
                 ordinal = imp.ordinal
-                encoding_option = chardet.detect(function)['encoding']
-                if encoding_option == None:
-                    continue
-                function = function.decode(encoding_option, 'ignore').replace("\u0000","")
+                function = function.decode().strip(' \t\r\n\0')
                 if dll not in library:
                     library.add(dll)
                 array.append({"library": dll, "address": address, "function": function, "ordinal" : ordinal})
@@ -239,34 +144,15 @@ def get_export_function( pe ) :
     array = []
     try:
         for exp in pe.DIRECTORY_ENTRY_EXPORT.symbols:
+            if exp.name == None :
+                continue
             # No dll
             address = hex(pe.OPTIONAL_HEADER.ImageBase + exp.address)
-            encoding_option = chardet.detect(exp.name)['encoding']
-            if encoding_option == None :
-                continue
-            function = exp.name.decode(encoding_option).replace("\u0000","")
+            function = exp.name.decode().strip(' \t\r\n\0')
             array.append({"address": address, "function": function})
     except:
         pass
     return array
-
-def get_apialert_info( pe ) :
-    alerts = set()
-    with open(_APIALERT, 'r') as f :
-        for line in f.readlines() :
-            alerts.add(line.strip())
-    apialert_found = set()
-    if hasattr(pe, 'DIRECTORY_ENTRY_IMPORT'):
-        for lib in pe.DIRECTORY_ENTRY_IMPORT:
-            for imp in lib.imports:
-                encoding_option = chardet.detect(imp.name)['encoding']
-                if encoding_option == None :
-                    continue
-                imp_name = imp.name.decode(encoding_option).replace("\u0000","")
-                if imp_name in alerts :
-                    apialert_found.add(imp_name)
-
-    return sorted(apialert_found)
 
 def get_anti_debug_info( file_data ) :
     rules = yara.compile(_ANTIDEBUG)
@@ -304,13 +190,13 @@ def get_fuzzy_hash( context ) :
     return ret
 
 def get_machine_info( pe ) :
-    return _MACHINE_TYPES.get(pe.FILE_HEADER.Machine, hex(pe.FILE_HEADER.Machine))
+    return hex(pe.FILE_HEADER.Machine)
 
 def get_size_of_optional_header( pe ) :
     return pe.FILE_HEADER.SizeOfOptionalHeader
 
 def get_characteristics( pe ) :
-    return _IMAGE_CHARACTERISTICS.get(pe.FILE_HEADER.Characteristics, hex(pe.FILE_HEADER.Characteristics))
+    return hex(pe.FILE_HEADER.Characteristics)
 
 def get_major_linker_version ( pe ) :
     return pe.OPTIONAL_HEADER.MajorLinkerVersion
@@ -337,7 +223,6 @@ def get_base_of_data( pe ) :
     try :
         return pe.OPTIONAL_HEADER.BaseOfData
     except :
-        # If Machine is 32bit
         return None
 
 def get_image_base(pe) :
@@ -380,7 +265,7 @@ def get_subsystem(pe) :
     return pe.OPTIONAL_HEADER.Subsystem
 
 def get_dll_characteristics(pe) :
-    return _DLL_CHARACTERISTICS.get(pe.OPTIONAL_HEADER.DllCharacteristics, hex(pe.OPTIONAL_HEADER.DllCharacteristics))
+    return hex(pe.OPTIONAL_HEADER.DllCharacteristics)
 
 def get_size_of_stack_reserve(pe) :
     return pe.OPTIONAL_HEADER.SizeOfStackReserve
@@ -448,8 +333,6 @@ def run( file_path ) :
     json_obj['pe_info']['import_function'] = get_import_function(pe)
     ## Export Function
     json_obj['pe_info']['export_function'] = get_export_function(pe)
-    ## API Alert Info
-    json_obj['pe_info']['apialert_info'] = get_apialert_info( pe )
     ## Machine
     json_obj['pe_info']['machine'] = get_machine_info( pe )
     ## SizeOfOptionalHeader
